053ac9a0a9cb821a74733c3968ca827a4adc18b1
jengleberg <jeffengleberg@hotmail.com>
2017-09-22 08:33:06 -0600

    adds initial html-css-js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+canvas.id     = "gameArea";
+canvas.width  = 720;
+canvas.height = 270;
+canvas.style.zIndex   = 8;
+
+canvas.style.position = "absolute";
