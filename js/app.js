71c45d9b70f5d75b67fd797c31562c06799e0f81
jengleberg <jeffengleberg@hotmail.com>
2017-09-24 17:49:07 -0600

    adds newJS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,65 @@
-var canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-canvas.id     = "gameArea";
-canvas.width  = 720;
-canvas.height = 270;
-canvas.style.zIndex   = 8;
+var beavis;
+var butthead;
 
-canvas.style.position = "absolute";
+function startGame() {
+	beavis = new component(40, 40, "red", 120, 270);
+    butthead = new component(40, 40, "blue", 120, 320);
+    myGameArea.start();
+    
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 800;
+        this.canvas.height = 400;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval (updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        });
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        });
+    },
+    clear: function() {
+    	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+    	ctx = myGameArea.context;
+    	ctx.fillStyle = color;
+    	ctx.fillRect(this.x, this.y, this.width, this.height);
+	};
+
+ this.newPos = function() {
+ 	this.x += this.speedX;
+ };
+}
+
+function updateGameArea() {
+	myGameArea.clear();
+	beavis.speedX = 0;
+    if (myGameArea.key && myGameArea.key == 65) {beavis.speedX = 3; }
+    beavis.newPos(); 
+    beavis.update();
+    butthead.speedX = 0;
+    if (myGameArea.key && myGameArea.key == 76) {butthead.speedX = 3; }
+    butthead.newPos(); 
+    butthead.update();
+
+ }
+function isWinner() {
+ 	if (beavis > myGameArea) {
+ 		alert("Beavis won!");
+ 	
+ 	}
+}
---
+++ jspractice.js
@@ -0,0 +1,53 @@
+window.onload = start;
+var beavis = document.getElementById('beavis');
+var butthead = document.getElementById('butthead');
+var container = document.getElementById('container');
+
+document.onkeydown = anim;
+
+
+
+var beavisLeft = 0;
+var buttheadLeft = 0;
+
+
+function anim(e) {
+
+if(e.keyCode == 39){
+	beavisLeft +=20;
+	beavis.style.left = beavisLeft + 'px';
+	if (beavisLeft >= 550){
+		alert("Beavis Wins!");
+		start();
+		
+		
+	}
+
+}
+
+
+
+if (e.keyCode == 37){
+	buttheadLeft +=20;
+	butthead.style.left = buttheadLeft + 'px';
+	if (buttheadLeft >= 550){
+		alert("Butthead Wins!");
+		start();
+	}
+	
+
+}
+
+
+
+}
+
+function newGame() {
+	var resetButton = document.getElementById('newGame');
+	resetButton.addEventListener("click", start);
+}
+
+function start() {
+   var beavisLeft = 0;
+   var buttheadleft = 0; 
+}
